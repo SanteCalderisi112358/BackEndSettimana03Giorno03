9eed1ecc07145766167e05903887c60f60a3590a
SanteCalderisi112358 <santecalderisi@gmail.com>
2023-07-13 16:30:55 +0200

    metodi get1,get2,get3,get4,get5 in EventoDAO

--- src/main/java/GestioneEventiRelazionaleDAO/EventoDAO.java
+++ src/main/java/GestioneEventiRelazionaleDAO/EventoDAO.java
@@ -6,14 +6,18 @@ import java.util.UUID;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
+import javax.persistence.NamedQuery;
 import javax.persistence.Query;
 
 import GestioneEventiRelazionaleEntities.Concerto;
 import GestioneEventiRelazionaleEntities.Evento;
 import GestioneEventiRelazionaleEntities.Genere;
+import GestioneEventiRelazionaleEntities.PartitaCalcio;
 import GestioneEventiRelazionaleUtilies.JPAUtilies;
 
-//@NamedQuery(name = "isInStreaming", query = "SELECT concerto FROM Concerto concerto WHERE concerto.inStreaming LIKE :isInStreaming")
+@NamedQuery(name = "partiteVinteInCasa", query = "SELECT partita FROM PartitaCalcio partita WHERE partita.golCasa>partita.golOspite")
+@NamedQuery(name = "partiteVInteInTrasferta", query = "SELECT partita FROM PartitaCalcio partita WHERE partita.golCasa<partita.golOspite")
+@NamedQuery(name = "partitePareggiate", query = "SELECT partita FROM PartitaCalcio partita WHERE partita.golCasa==partita.golOspite")
 public class EventoDAO {
 	private EntityManager em;
 	private EntityManagerFactory emf = JPAUtilies.getEntityManagerFactory();
@@ -56,7 +60,7 @@ public class EventoDAO {
 		try {
 			Query query = em.createNamedQuery(
 					"SELECT concerto FROM Concerto concerto WHERE concerto.inStreaming LIKE :isInStreaming");
-			query.getParameter("isInStreaming");
+			query.setParameter("isInStreaming", isInStreaming);
 			@SuppressWarnings("unchecked")
 			List<Concerto> concertiInStreaming = query.getResultList();
 			if (isInStreaming) {
@@ -71,24 +75,81 @@ public class EventoDAO {
 		}
 	}
 
+	@SuppressWarnings("unchecked")
 	public List<Concerto> getConcertiPerGenere(Genere genere) {
 		EntityManager em = emf.createEntityManager();
 		try {
-			Query query = em
-					.createNamedQuery("SELECT concerto FROM Concerto concerto WHERE concerto.genere LIKE :genere");
-			query.getParameter("genere");
-			@SuppressWarnings("unchecked")
-			List<Concerto> concertiInStreaming = query.getResultList();
-			if (concertiInStreaming != null) {
-				return concertiInStreaming;
+			Query query = em.createQuery("SELECT concerto FROM Concerto concerto WHERE concerto.genere = :genere");
+			query.setParameter("genere", genere);
+			List<Concerto> concertiPerGenere = query.getResultList();
+
+			if (concertiPerGenere != null) {
+				return concertiPerGenere;
 			} else {
 				System.out.println("Non esistono concerti del genere " + genere);
 				return null;
 			}
+		} finally {
+			em.close();
+		}
+	}
 
+	@SuppressWarnings("unchecked")
+	public List<PartitaCalcio> getPartiteVinteInCasa() {
+		EntityManager em = emf.createEntityManager();
+		try {
+			Query query = em.createQuery("partiteVinteInCasa");
+
+			List<PartitaCalcio> partiteVinteInCasa = query.getResultList();
+
+			if (partiteVinteInCasa != null) {
+				return partiteVinteInCasa;
+			} else {
+				System.out.println("Nessuna squadra ha vinto in casa");
+				return null;
+			}
+		} finally {
+			em.close();
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<PartitaCalcio> getPartiteVinteInTrasferta() {
+		EntityManager em = emf.createEntityManager();
+		try {
+			Query query = em.createQuery("partiteVinteInTrasferta");
+
+			List<PartitaCalcio> partiteVinteInTrasferta = query.getResultList();
+
+			if (partiteVinteInTrasferta != null) {
+				return partiteVinteInTrasferta;
+			} else {
+				System.out.println("Nessuna squadra ha vinto in trasferta");
+				return null;
+			}
 		} finally {
 			em.close();
 		}
 
-}
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<PartitaCalcio> getpartitePareggiate() {
+		EntityManager em = emf.createEntityManager();
+		try {
+			Query query = em.createQuery("partitePareggiate");
+
+			List<PartitaCalcio> partitePareggiate = query.getResultList();
+
+			if (partitePareggiate != null) {
+				return partitePareggiate;
+			} else {
+				System.out.println("Nessuna partita è terminata con un pareggio");
+				return null;
+			}
+		} finally {
+			em.close();
+		}
+	}
+
 }
